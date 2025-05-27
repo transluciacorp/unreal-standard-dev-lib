7b72f7eeea729887cfe99085ef01014f5f8953fb
Thanut Panichyotai <thanut@translucia.com>
2025-05-27 13:49:00 +0700

    Add a new standard develop library for bitmask. Increase version.

---
+++ Source/StandardDevLib/Private/StandardBitmaskUtils.cpp
@@ -0,0 +1,3 @@
+﻿// Copyright 2024-2025 Lumin8 & Translucia Limited - All Rights Reserved.
+
+#include "StandardBitmaskUtils.h"
---
+++ Source/StandardDevLib/Public/StandardBitmaskUtils.h
@@ -0,0 +1,32 @@
+﻿// Copyright 2024-2025 Lumin8 & Translucia Limited - All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "StandardBitmaskUtils.generated.h"
+
+/**
+ * This is a standard utility class for bitmask operations in Unreal Engine.
+ */
+UCLASS()
+class STANDARDDEVLIB_API UStandardBitmaskUtils : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+public:
+	/**
+	 * Checks if a specific flag is set in a bitmask represented by an integer.
+	 * @param Flags The bitmask integer to check against.
+	 * @param Flag The specific flag to check, represented as an integer.
+	 * @return True if the flag is set, false otherwise.
+	 */
+	UFUNCTION(
+		BlueprintPure,
+		Category = "Standard Development Library|Bitmask",
+		DisplayName = "Has Flag (Bitmask Integer & Byte)")
+	static bool HasFlag(const int32 Flags, const uint8 Flag)
+	{
+		return ((1 << Flag) & Flags) != 0;
+	}
+};
