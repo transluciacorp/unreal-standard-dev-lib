1126767b1be3f3fcace3ad3f9d23878118824099
Thanut Panichyotai <thanut@translucia.com>
2025-06-20 13:44:03 +0700

    Add StandardStringUtils class with string utility functions and update plugin version to 1.2.0

---
+++ Source/StandardDevLib/Private/StandardStringUtils.cpp
@@ -0,0 +1,27 @@
+﻿// Copyright 2024-2025 Lumin8 & Translucia Limited - All Rights Reserved.
+
+#include "StandardStringUtils.h"
+
+FString FStandardStringUtils::AlphabeticUppercaseCharacterSet = TEXT("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+FString FStandardStringUtils::AlphabeticLowercaseCharacterSet = TEXT("abcdefghijklmnopqrstuvwxyz");
+FString FStandardStringUtils::NumericCharacterSet = TEXT("0123456789");
+FString FStandardStringUtils::HexadecimalUppercaseCharacterSet = TEXT("ABCDEF");
+FString FStandardStringUtils::HexadecimalLowercaseCharacterSet = TEXT("abcdef");
+
+FString FStandardStringUtils::AlphaNumericCharacterSet = AlphabeticUppercaseCharacterSet + AlphabeticLowercaseCharacterSet + NumericCharacterSet;
+FString FStandardStringUtils::HexadecimalNumericCharacterSet = HexadecimalLowercaseCharacterSet + NumericCharacterSet;
+FString FStandardStringUtils::HexadecimalNumericUppercaseCharacterSet = HexadecimalUppercaseCharacterSet + NumericCharacterSet;
+
+FString FStandardStringUtils::GetRandomString(const int32 Length, const FString& CharacterSet)
+{
+	checkf(Length > 0, TEXT("Length must be greater than 0"));
+	checkf(!CharacterSet.IsEmpty(), TEXT("CharacterSet must not be empty"));
+	FString Result;
+	Result.Reserve(Length);
+	for (int32 CurrentLength = 0; CurrentLength < Length; ++CurrentLength)
+	{
+		const int32 Index = FMath::RandRange(0, CharacterSet.Len() - 1);
+		Result.AppendChar(CharacterSet[Index]);
+	}
+	return Result;
+}
---
+++ Source/StandardDevLib/Public/StandardStringUtils.h
@@ -0,0 +1,46 @@
+﻿// Copyright 2024-2025 Lumin8 & Translucia Limited - All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "StandardStringUtils.generated.h"
+
+/**
+ * Utility class for string operations.
+ * This class provides static methods for generating random strings and other string-related utilities.
+ */
+class STANDARDDEVLIB_API FStandardStringUtils
+{
+public:
+	static FString AlphabeticUppercaseCharacterSet;
+	static FString AlphabeticLowercaseCharacterSet;
+	static FString NumericCharacterSet;
+	static FString HexadecimalUppercaseCharacterSet;
+	static FString HexadecimalLowercaseCharacterSet;
+
+	static FString AlphaNumericCharacterSet;
+	static FString HexadecimalNumericCharacterSet;
+	static FString HexadecimalNumericUppercaseCharacterSet;
+
+	static FString GetRandomString(const int32 Length, const FString& CharacterSet);
+};
+
+/**
+ * Blueprint function library for string operations.
+ */
+UCLASS()
+class STANDARDDEVLIB_API UStringBlueprintFunctionLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(
+		BlueprintPure,
+		Category = "Translucia|String Operations",
+		DisplayName = "Get Random String By Character Set")
+	static FString GetRandomString(const int32 Length, const FString& CharacterSet)
+	{
+		return FStandardStringUtils::GetRandomString(Length, CharacterSet);
+	}
+};
